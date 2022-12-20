4b27334457f4d90f8b42f639c13f41340cc1dc99
Richard Sundqvist <richard.sundqvist@live.se>
2022-12-20 22:28:44 +0100

    Add project flag to run-docker-dvdrental.sh

--- run-docker-dvdrental.sh
+++ run-docker-dvdrental.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker compose -f tests/dvdrental/docker/docker-compose.yml up --wait
+docker compose -p dvdrental -f tests/dvdrental/docker/docker-compose.yml up --wait
 echo "Sleeping for 10 sec before verification.."
 sleep 10
 docker run --network=host --rm rsundqvist/sakila-preload:db-tests
