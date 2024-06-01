09cd9bc940ce2aa135cfe94619244a78435cf8d5
Richard Sundqvist <richard.sundqvist@live.se>
2024-06-01 19:28:39 +0200

    Add @deprecated_params

--- src/id_translation/_compat.py
+++ src/id_translation/_compat.py
@@ -1,8 +1,12 @@
 """Vendored functions."""
 
+from functools import wraps
 from os import PathLike
 from pathlib import Path
-from typing import TypeAlias
+from typing import Callable, ParamSpec, TypeAlias, TypeVar
+from warnings import warn
+
+from rics.misc import tname
 
 # TODO(0.11.0): any_path_to_path(p, postprocessor=...)
 PathLikeType: TypeAlias = str | PathLike[str] | Path
@@ -19,3 +23,42 @@ except ImportError:
     # TODO(0.11.0): Drop this.
     fmt_perf = performance.format_perf_counter
     fmt_sec = performance.format_seconds
+
+
+P = ParamSpec("P")
+T = TypeVar("T")
+
+PARAMS = (
+    ("copy", "inplace"),
+    # ("max_fails", "maximal_untranslated_fraction"),
+)
+WARNED: set[str] = set()  # Minimize the amount of warnings - once per param is enough.
+
+
+def deprecated_params(__func: Callable[P, T], /) -> Callable[P, T]:
+    @wraps(__func)
+    def wrap(*args: P.args, **kwargs: P.kwargs) -> T:
+        for new, old in PARAMS:
+            if old in kwargs:
+                if new in kwargs:
+                    name = tname(__func, prefix_classname=True)
+                    msg = f"Ambiguous call: {name}() does not accept both `{old}` and `{new}`."
+                    raise TypeError(msg)
+
+                if old not in WARNED:
+                    WARNED.add(old)
+
+                    name = tname(__func, prefix_classname=True)
+                    msg = f"{name}(): The `{old}` parameter is deprecated; use `{new}` instead."
+                    warn(message=msg, category=DeprecationWarning, stacklevel=2)
+
+                value = kwargs.pop(old)
+
+                if old == "inplace":
+                    value = not value
+
+                kwargs[new] = value
+
+        return __func(*args, **kwargs)
+
+    return wrap
