51169cf8c26bb5ce113561f86f0ccfe6ed7db71b
Richard Sundqvist <richard.sundqvist@live.se>
2025-07-20 16:52:03 +0000

    Bump version: 0.15.1 → 0.15.1.dev1

--- src/id_translation/__init__.py
+++ src/id_translation/__init__.py
@@ -30,7 +30,7 @@ __all__ = [
     "__version__",  # Make MyPy happy
 ]
 
-__version__ = "0.15.1"
+__version__ = "0.15.1.dev1"
 
 _logging.getLogger(__name__).addHandler(_logging.NullHandler())
 del _logging
