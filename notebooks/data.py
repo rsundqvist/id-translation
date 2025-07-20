9a3a48b976ee11ef6caf55b21b06b75d41eb0377
Richard Sundqvist <richard.sundqvist@live.se>
2025-07-20 13:53:37 +0000

    docs: Update notebooks

--- notebooks/data.py
+++ notebooks/data.py
@@ -6,7 +6,8 @@ from pathlib import Path
 import pandas as pd
 from rics.misc import get_local_or_remote
 
-_LOCAL_ROOT = Path(__file__).parent.joinpath("data-cache")
+_LOCAL_ROOT = Path.home() / ".id-translation/notebooks/cache/"
+_LOCAL_ROOT.mkdir(parents=True, exist_ok=True)
 
 
 def _fix_ids(df):
@@ -15,7 +16,9 @@ def _fix_ids(df):
 
     columns = _get_id_columns(df)
     for col in columns:
-        df[f"int_id_{col}"] = df[col].str.replace(re.compile("^[a-zA-Z]+?([0-9]+)$"), integer_group).astype(int)
+        df[f"int_id_{col}"] = (
+            df[col].str.replace(re.compile("^[a-zA-Z]+?([0-9]+)$"), integer_group, regex=True).astype(int)
+        )
 
 
 def _get_id_columns(df):
@@ -29,16 +32,16 @@ def clean_and_fix_ids(input_path) -> pd.DataFrame:
 
 
 def load_pickle(input_path):
-    df = pd.read_csv(input_path, sep="\t", header=0, engine="c")
+    df = pd.read_csv(input_path, sep="\t", header=0, engine="c", low_memory=False)
     any_nan = (df == "\\N").any(axis=1) | df.isna().any(axis=1)
     df = df[~any_nan]
-    df = df.apply(pd.to_numeric, errors="ignore")
+    df = df.apply(pd.to_numeric)
     return df
 
 
-def load_imdb(dataset, postprocessor=clean_and_fix_ids, **kwargs):
+def load_imdb(dataset):
     remote_root = "https://datasets.imdbws.com"
     file = f"{dataset}.tsv.gz"
-    path = get_local_or_remote(file, remote_root, _LOCAL_ROOT, postprocessor=postprocessor, **kwargs)
+    path = get_local_or_remote(file, remote_root=remote_root, local_root=_LOCAL_ROOT, postprocessor=clean_and_fix_ids)
     df = pd.read_pickle(path)
     return df, _get_id_columns(df)
