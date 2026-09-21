14c6f3e6e1f70576ac0b0de127476858a4ccecba
Richard Sundqvist <richard.sundqvist@live.se>
2026-09-21 17:18:47 +0200

    docker: Don't start the s3-admin container by default

--- run-docker-dvdrental.sh
+++ run-docker-dvdrental.sh
@@ -3,7 +3,9 @@
 set -eax
 
 docker compose -v -f tests/dvdrental/docker/docker-compose.yml down
-docker compose -p dvdrental -f tests/dvdrental/docker/docker-compose.yml up --wait
-echo "Sleeping for 10 sec before verification.."
+
+docker compose -p id-translation-tests -f tests/dvdrental/docker/docker-compose.yml --profile=$"${1}" up --wait
+printf "\033[92mSleeping for 10 sec before verification..\033[0m\n"
+
 sleep 10
 docker run --network=host --rm rsundqvist/sakila-preload:db-tests
